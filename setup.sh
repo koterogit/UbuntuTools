0fbd47d0182a3e84741b2ea47cbdeac4b69eebc6
dev <dev>
2020-05-09 21:33:45 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt install zsh
 zsh --version
 sudo chsh -s $(which zsh)
 sudo apt install git
+sudo apt install fcitx fcitx-mozc
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo please logout...
