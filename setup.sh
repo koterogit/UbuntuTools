52579a7663f4fce26670226fc28106ce2ad08250
dev <dev>
2020-05-10 10:31:46 +0900

    update

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@
 sudo apt install zsh
 zsh --version
 sudo chsh -s $(which zsh)
+
 sudo apt install git
 sudo apt install fcitx fcitx-mozc
+sudo apt install gnome-tweak-tool
+
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo please logout...
