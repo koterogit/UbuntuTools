5e535c0670015e94fb40b186a8f8860d4a697400
dev <dev>
2020-05-10 11:36:09 +0900

    update

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ sudo chsh -s $(which zsh)
 sudo apt install git
 sudo apt install fcitx fcitx-mozc
 sudo apt install gnome-tweak-tool
+sudo apt install adb
+sudo usermod -aG plugdev $LOGNAME
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
