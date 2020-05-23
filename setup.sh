bbec1cbc2a12412793ab9250f2e2a3df39ff3373
dev <dev>
2020-05-23 17:49:13 +0900

    update

--- setup.sh
+++ setup.sh
@@ -7,9 +7,15 @@ sudo chsh -s $(which zsh)
 sudo apt install git
 sudo apt install fcitx fcitx-mozc
 sudo apt install gnome-tweak-tool
-sudo apt install adb
+sudo apt install adb fastboot
 sudo usermod -aG plugdev $LOGNAME
 
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# jupyterlab
+sudo apt install pipenv
+pipenv install jupyterlab
+
+# prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+echo 'source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"' >> ~/.zshrc 
 
 echo please logout...
